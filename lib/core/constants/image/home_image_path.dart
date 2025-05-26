c46eeeee1f1f536192090c68abac0204652a454e
shahedroks <shahedroks@gmail.com>
2025-05-26 16:20:39 +0600

    add image path

---
+++ lib/core/constants/image/home_image_path.dart
@@ -0,0 +1,3 @@
+String movie1Image = "";
+String movie2Image = "";
+String movie3Image = "";
--- lib/futures/home/screens/home_page.dart
+++ lib/futures/home/screens/home_page.dart
@@ -13,6 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  PageController _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +35,50 @@ class _HomePageState extends State<HomePage> {
                       Text("see_all".tr,),
                     ],
                   ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    height: 150,
+                    child: Obx(() => PageView.builder(
+                      controller: _pageController,
+                      itemCount: 4,
+                      onPageChanged: (index) {
+                        _homeController.currentPage.value = index;
+                      },
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              ,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      },
+                    )),
+                  ),
+                  const SizedBox(height: 8),
+                  Obx(() => Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      ImageController.bannerImages.length,
+                          (index) => AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        margin: EdgeInsets.symmetric(horizontal: 4),
+                        width: _homeController.currentPage.value == index ? 12 : 8,
+                        height: _homeController.currentPage.value == index ? 12 : 8,
+                        decoration: BoxDecoration(
+                          color: _homeController.currentPage.value == index
+                              ? Colors.green
+                              : Colors.grey,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                  )),
                   // PageView(children: [
                   //
                   // ],)
