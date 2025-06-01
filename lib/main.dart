58765a51b665e4cc7857c57ef7a374be5dd57d08
Palash-Chandra-Roy <palashtpi21@gmail.com>
2025-06-01 22:47:23 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_ticket_booging/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
