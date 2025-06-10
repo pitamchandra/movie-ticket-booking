d5e3299b42a4bd18a91547a621f85d89c56329a9
shahedroks <shahedroks@gmail.com>
2025-06-10 15:17:31 +0600

    stt added

--- lib/futures/home/widget/home_search_bar.dart
+++ lib/futures/home/widget/home_search_bar.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
-class HomeSearchBar extends StatelessWidget {
+class HomeSearchBar extends StatefulWidget {
   const HomeSearchBar({super.key});
 
   @override
+  State<HomeSearchBar> createState() => _HomeSearchBarState();
+}
+
+class _HomeSearchBarState extends State<HomeSearchBar> {
+
+  @override
   Widget build(BuildContext context) {
+    final TextEditingController _searchController = TextEditingController(text: _text);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: TextFormField(
+        controller: _searchController,
         decoration: InputDecoration(
           filled: true,
           fillColor: Colors.grey,
@@ -17,8 +26,47 @@ class HomeSearchBar extends StatelessWidget {
           ),
           hintText: "Search",
           prefixIcon: Icon(Icons.search),
+          suffixIcon: InkWell(
+            onTap: (){
+              startSpeeching();
+            },
+              child: CircleAvatar(child: Icon(_isListening ? Icons.mic: Icons.mic_off)))
         ),
       ),
     );
+  }
+
+  late stt.SpeechToText _speech;
+
+  bool _isListening = false;
+
+  String _text = 'Press the button and start speaking';
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+  startSpeeching (){
+    _listen();
+  }
+  Future<void> _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('Status: $val'),
+        onError: (val) => print('Error: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
   }
 }
